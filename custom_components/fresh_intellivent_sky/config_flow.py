0e66b4b76c17d57398b16b1800a61999960c78c6
Spit68 <brian.nielsen@spittie.dk>
2026-07-18 15:00:11 +0200

    Add files via upload

---
+++ custom_components/fresh_intellivent_sky/config_flow.py
@@ -0,0 +1,253 @@
+"""Config flow for Fresh Intellivent Sky integration."""
+from __future__ import annotations
+
+import dataclasses
+import logging
+from typing import Any
+
+import voluptuous as vol
+from bleak import BleakError
+from homeassistant.components import bluetooth
+from homeassistant.components.bluetooth import (
+    BluetoothServiceInfo,
+    async_discovered_service_info,
+)
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.const import CONF_ADDRESS
+from homeassistant.data_entry_flow import FlowResult
+from .fresh_intellivent import FreshIntelliVent
+from .helpers import validated_authentication_code
+
+from .const import (
+    CONF_AUTH_KEY,
+    DOMAIN,
+    NAME,
+    AUTH_MANUAL,
+    AUTH_FETCH,
+    NO_AUTH,
+    AUTH_CODE_ONLY_ZERO,
+    AUTH_CODE_EMPTY,
+    TIMEOUT,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class Discovery:
+    """A discovered bluetooth device."""
+
+    name: str
+    discovery_info: BluetoothServiceInfo
+    device: FreshIntelliVent
+
+
+def get_name(device: FreshIntelliVent) -> str:
+    """Generate name with identifier for device."""
+    return f"{device.manufacturer} {device.name}"
+
+
+class FreshIntelliventSkyDeviceUpdateError(Exception):
+    """Custom error class for device updates."""
+
+
+class FreshIntelliventSkyConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Fresh Intellivent Sky."""
+
+    VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize the config flow."""
+        self._discovered_device: Discovery | None = None
+        self._discovered_devices: dict[str, Discovery] = {}
+
+    async def _get_device_data(
+        self, discovery_info: BluetoothServiceInfo
+    ) -> FreshIntelliVent:
+        ble_device = bluetooth.async_ble_device_from_address(
+            self.hass, discovery_info.address
+        )
+        if ble_device is None:
+            raise FreshIntelliventSkyDeviceUpdateError("No ble_device")
+
+        client = FreshIntelliVent(ble_device=ble_device)
+        error = None
+
+        try:
+            await client.connect(timeout=TIMEOUT)
+            await client.fetch_device_information()
+
+        except BleakError:
+            error = FreshIntelliventSkyDeviceUpdateError("Failed getting device data")
+        except Exception as err:
+            _LOGGER.exception(
+                "Unknown error occurred while getting data from %s",
+                discovery_info.address,
+            )
+            error = err
+        finally:
+            await client.disconnect()
+
+        if error is not None:
+            raise error
+
+        return client
+
+    async def async_step_bluetooth(
+        self, discovery_info: BluetoothServiceInfo
+    ) -> FlowResult:
+        """Handle the bluetooth discovery step."""
+        await self.async_set_unique_id(discovery_info.address)
+        self._abort_if_unique_id_configured()
+
+        try:
+            device = await self._get_device_data(discovery_info)
+        except FreshIntelliventSkyDeviceUpdateError:
+            return self.async_abort(reason="cannot_connect")
+        except Exception:
+            return self.async_abort(reason="unknown")
+
+        name = get_name(device)
+        self.context["title_placeholders"] = {"name": name}
+        self._discovered_device = Discovery(name, discovery_info, device)
+
+        return await self.async_step_bluetooth_confirm()
+
+    async def async_step_bluetooth_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm discovery."""
+        if user_input is None:
+            self._set_confirm_only()
+            return self.async_show_form(
+                step_id="bluetooth_confirm",
+                description_placeholders=self.context["title_placeholders"],
+            )
+
+        return await self.async_step_auth_method()
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the user step to pick discovered device."""
+        if user_input is not None:
+            address = user_input[CONF_ADDRESS]
+            await self.async_set_unique_id(address, raise_on_progress=False)
+            self._abort_if_unique_id_configured()
+            discovery = self._discovered_devices[address]
+
+            self.context["title_placeholders"] = {
+                "name": discovery.name,
+            }
+            self._discovered_device = discovery
+
+            return await self.async_step_auth_method()
+
+        current_addresses = self._async_current_ids()
+        
+        for discovery_info in async_discovered_service_info(self.hass):
+            address = discovery_info.address
+   
+            if address in current_addresses or address in self._discovered_devices:
+                continue
+
+            if discovery_info.name not in NAME:
+                continue
+
+            try:
+                device = await self._get_device_data(discovery_info)
+            except FreshIntelliventSkyDeviceUpdateError:
+                return self.async_abort(reason="cannot_connect")
+            except Exception:
+                return self.async_abort(reason="unknown")
+            name = get_name(device)
+            self._discovered_devices[address] = Discovery(name, discovery_info, device)
+
+        if not self._discovered_devices:
+            return self.async_abort(reason="no_devices_found")
+
+        titles = {
+            address: get_name(discovery.device)
+            for (address, discovery) in self._discovered_devices.items()
+        }
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {vol.Required(CONF_ADDRESS): vol.In(titles)},
+            ),
+        )
+
+    async def async_step_auth_method(
+        self, user_input: dict[str, Any] | None = None, error: str | None = None
+    ) -> FlowResult:
+        """Get auth key."""
+        errors = {}
+        if error is not None:
+            errors["base"] = error
+            return self.async_show_form(
+                step_id="auth_method", errors=errors, last_step=False
+            )
+        return self.async_show_menu(
+            step_id="auth_method", menu_options=[AUTH_FETCH, AUTH_MANUAL, NO_AUTH]
+        )
+
+    async def async_step_no_auth(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """No auth key."""
+        return self.async_create_entry(
+            title=self.context["title_placeholders"]["name"],
+            data={},
+        )
+
+    async def async_step_auth_manual(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Write auth key."""
+        errors = {}
+        if user_input is not None:
+            auth_key = user_input.get(CONF_AUTH_KEY)
+
+            try:
+                if auth_key is not None:
+                    validated_authentication_code(auth_key)
+            except (TypeError, ValueError):
+                errors["base"] = "invalid_auth_key"
+            else:
+                return self.async_create_entry(
+                    title=self.context["title_placeholders"]["name"],
+                    data={CONF_AUTH_KEY: auth_key},
+                )
+
+        return self.async_show_form(
+            step_id="auth_manual",
+            description_placeholders=self.context["title_placeholders"],
+            data_schema=vol.Schema({vol.Optional(CONF_AUTH_KEY): str}),
+            errors=errors,
+        )
+
+    async def async_step_auth_fetch(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Fetch auth key."""
+        code = None
+        try:
+            await self._discovered_device.device.connect(timeout=TIMEOUT)
+            code = await self._discovered_device.device.fetch_authentication_code()
+            code = validated_authentication_code(code)
+        except Exception:
+            pass
+        finally:
+            await self._discovered_device.device.disconnect()
+
+        if code is None:
+            return await self.async_step_auth_method(error=AUTH_CODE_EMPTY)
+
+        elif code == bytearray(b"\x00\x00\x00\x00"):
+            return await self.async_step_auth_method(error=AUTH_CODE_ONLY_ZERO)
+
+        else:
+            return self.async_create_entry(
+                title=self.context["title_placeholders"]["name"],
+                data={CONF_AUTH_KEY: code.hex()},
+            )
---
+++ custom_components/fresh_intellivent_sky/const.py
@@ -0,0 +1,33 @@
+"""Constants for the Fresh Intellivent Sky integration."""
+
+DOMAIN = "fresh_intellivent_sky"
+
+NAME = ["Intellivent SKY", "Intellivent ICE"]
+
+TIMEOUT = 30.0
+
+AUTH_MANUAL = "auth_manual"
+AUTH_FETCH = "auth_fetch"
+NO_AUTH = "no_auth"
+AUTH_CODE_ONLY_ZERO = "auth_code_only_zero"
+AUTH_CODE_EMPTY = "auth_code_empty"
+
+CONF_AUTH_KEY = "auth_key"
+
+BOOST_UPDATE = "boost_update"
+BOOST_REMAINING_KEY = "boost_remaining"
+PAUSE_REMAINING_KEY = "pause_remaining"
+CONSTANT_SPEED_UPDATE = "constant_speed_update"
+PAUSE_UPDATE = "pause_update"
+
+AIRING_MODE_UPDATE = "airing_update"
+HUMIDITY_MODE_UPDATE = "humidity_mode_update"
+LIGHT_AND_VOC_MODE_UPDATE = "light_and_voc_mode_update"
+TIMER_MODE_UPDATE = "timer_mode_update"
+VOC_MODE_UPDATE = "voc_mode_update"
+
+DETECTION_KEY = "detection"
+ENABLED_KEY = "enabled"
+DELAY_KEY = "delay"
+RPM_KEY = "rpm"
+MINUTES_KEY = "minutes"
