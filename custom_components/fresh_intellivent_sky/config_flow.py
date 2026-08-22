09a24b54ca811b535531359ad8ddc4c1dee8054b
Spit68 <brian.nielsen@spittie.dk>
2026-08-22 14:34:34 +0200

    Add files via upload

--- custom_components/fresh_intellivent_sky/config_flow.py
+++ custom_components/fresh_intellivent_sky/config_flow.py
@@ -145,7 +145,7 @@ class FreshIntelliventSkyConfigFlow(ConfigFlow, domain=DOMAIN):
 
         current_addresses = self._async_current_ids()
         
-        for discovery_info in async_discovered_service_info(self.hass):
+        for discovery_info in list(async_discovered_service_info(self.hass)):
             address = discovery_info.address
    
             if address in current_addresses or address in self._discovered_devices:
